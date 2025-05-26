142b4e49e341187ad3c63995d143aa93553ab238
Johnson A <148605244+Johnson732@users.noreply.github.com>
2025-05-26 18:43:24 +0530

    Add files via upload

---
+++ scripts/show-files-folders.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+#################################################
+#
+# Author: Johnson
+# Date: 26-05-25
+#
+# Script to show files and folders recursively
+################################################
+
+
+if [ -z "$1" ]; then
+	echo "Usage: $0 <directory>"
+	exit 1
+
+fi
+
+DIRECTORY=$1
+FILE="files.txt"
+
+echo "The files and folders in $DIRECTORY are"
+
+find "$DIRECTORY" > $FILE
